7931876c9ecb78fe5e35b2cca0620c9189bf4821
VinBdev <vbpod77@gmail.com>
2022-06-13 18:13:59 +0000

    Deployment part 2

--- boutique_ado_v2/settings.py
+++ boutique_ado_v2/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vb-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -112,20 +112,21 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado_v2.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://ttwftctkkunlpi:65e8dec4ca011b9718ddaed898e7e4de50788ccd4463371940545562fb545bee@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/ddamb1ssq0nmq8')
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
